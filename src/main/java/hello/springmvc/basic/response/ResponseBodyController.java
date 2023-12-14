3629e693546a7067bebc73fe1e931018aae5d888
gialeee <gia.lee@gmail.com>
2023-12-14 15:38:34 +0900

    [ResponseBodyV3] @ResponseBody 애노테이션과 String 반환값으로 응답 처리

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -1,11 +1,14 @@
 package hello.springmvc.basic.response;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -22,4 +25,11 @@ public class ResponseBodyController {
     public ResponseEntity<String> responseBodyV2() {
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
 }
