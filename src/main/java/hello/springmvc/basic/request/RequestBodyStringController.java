3754001689055114384d7ddc7214b9cee441032c
gialeee <gia.lee@gmail.com>
2023-12-14 14:40:25 +0900

    [RequestBodyStringV3] 스프링의 HttpEntity로 요청/응답 받기

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -4,9 +4,14 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,4 +40,30 @@ public class RequestBodyStringController {
 
         responseWriter.write("ok");
     }
+
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 응답에서도 HttpEntity 사용 가능
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+
+        // Spring에서 제공하는 HttpEntity를 사용해서 헤더/바디 정보 편리하게 조회
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+
+        return new HttpEntity<>("ok");
+        
+         /*
+         // HttpEntity를 상속받는 RequestEntity, ResponseEntity도 사용 가능
+         public ResponseEntity<String> requestBodyStringV3(RequestEntity<String> httpEntity) {
+              ...
+              return new ResponseEntity<String>("ok", HttpStatus.CREATED); }*/
+    }
+
 }
