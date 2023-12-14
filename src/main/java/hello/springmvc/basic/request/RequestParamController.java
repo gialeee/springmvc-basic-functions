031dd928abf797de14947af795607a4e3566984b
gialeee <gia.lee@gmail.com>
2023-12-14 13:34:53 +0900

    @RequestParam V1, V2, V3, V4 메서드 추가

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,11 +5,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
 @Slf4j
-@Controller
+@Controller // @RestController가 아닌 경우 아래 메서드에 @ResponseBody 애노테이션 추가해줘야 String 메세지 바디에 반환
 public class RequestParamController {
 
     @RequestMapping("/request-param-v1")
@@ -19,5 +21,36 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) { // String, int, Integer 등의 단순타입이면 @RequestParam 생략 가능
+
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
     }
 }
