d50577239931c85bff51f07a9b7e33d3dd883e04
gialeee <gia.lee@gmail.com>
2023-12-14 15:26:02 +0900

    [ResponseViewV3] void 반환, @RequestMapping URL을 논리뷰 이름으로 사용

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -23,5 +23,13 @@ public class ResponseViewController {
         return "response/hello";
     }
 
-
+    /**
+     * @Controller를 사용하고, HTTP 메시지 바디 처리 파라미터(HttpServletResponse, OutputStream(Writer) 등)가 없으면
+     * 요청 URL을 참고해서 논리뷰 이름으로 사용
+     * - 권장 X (명시성 떨어짐, 사용불가한 경우 발생)
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
 }
